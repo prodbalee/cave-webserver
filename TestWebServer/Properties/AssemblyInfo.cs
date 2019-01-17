443d787cfe7a245a183f6eede3a5eadc73157300
Andreas Rohleder <andreas@rohleder.cc>
2019-01-17 13:56:16 +0100

    Release 1.0.0

--- TestWebServer/Properties/AssemblyInfo.cs
+++ TestWebServer/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("458435c2-d85a-49c1-9545-a7f2310d13a6")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-$CONF$-$DATETIME$")]
-[assembly: AssemblyFileVersion("2019.117.1217.855")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyFileVersion("2019.117.1244.842")]
