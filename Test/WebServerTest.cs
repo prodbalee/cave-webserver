fd1a29692037638e0650d0e334fa60e7fe4e54b2
Gernot Lenkner <g.lenkner@outlook.de>
2019-02-06 11:37:57 +0100

    added more tests. + basic test data struct with one value + basic auth test

--- Test/WebServerTest.cs
+++ Test/WebServerTest.cs
@@ -1,6 +1,9 @@
 using System.IO;
 using System.Net;
 using System.Threading;
+using Cave;
+using Cave.Auth;
+using Cave.Data;
 using Cave.Net;
 using Cave.Web;
 using NUnit.Framework;
@@ -9,6 +12,15 @@ namespace Test
 {
     public class WebServerTest
     {
+        [Table]
+        struct TestData
+        {
+            [Field(Flags =Cave.FieldFlags.ID)]
+            public long ID;
+            [Field]
+            public string Value;
+        }
+
         class TestPages
         {
             [WebPage(Paths ="testpage")]
@@ -20,8 +32,17 @@ namespace Test
             [WebPage(Paths = "testecho")]
             public void TestEcho(WebData webData, string value)
             {
-                webData.Result.AddMessage(webData.Method, "Echo success: ", value);
+                webData.Result.AddMessage(webData.Method, "Echo success");
+                TestData td = new TestData { ID = 1,  Value = value};
+                webData.Result.AddStruct<TestData>(td);
             }
+
+            [WebPage(Paths = "basicauth", AuthType =WebServerAuthType.Basic)]
+            public void TestBasicAuth(WebData webData)
+            {
+                webData.Result.AddMessage(webData.Method, "Basic Auth success: ");
+            }
+
 
         }
 
@@ -33,6 +54,11 @@ namespace Test
             Server.EnableExplain = true;
             Server.EnableFileListing = true;
             Server.SessionMode = WebServerSessionMode.Cookie;
+
+            User user;
+            EmailAddress email;
+
+            Server.AuthTables.CreateUser("user", "user@localhost", "password", UserState.Confirmed, 10, out user, out email);
 
             Server.StaticFilesPath = Directory.GetCurrentDirectory();
 
@@ -54,5 +80,41 @@ namespace Test
             HttpWebResponse response = (HttpWebResponse) request.GetResponse();
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
+
+        [Test]
+        public void GetTestPageXML()
+        {
+            XmlRequest request = XmlRequest.Prepare("http://localhost:8080", "testpage");
+            WebMessage message = request.Get();
+            Assert.AreEqual(HttpStatusCode.OK, message.Code);
+            Assert.AreEqual("Test Page success", message.Content);
+        }
+
+        [Test]
+        public void GetEchoXML()
+        {
+            XmlRequest request = XmlRequest.Prepare("http://localhost:8080", "testecho", "value=teststring");
+            WebMessage message = request.Get();
+            Assert.AreEqual(HttpStatusCode.OK, message.Code);
+            Assert.AreEqual("Echo success", message.Content);
+            var dataTable = request.Result.GetTable<TestData>();
+            Assert.AreEqual(1, dataTable.RowCount);
+            TestData data = dataTable.GetStruct("ID", 1);
+            Assert.AreEqual(1, data.ID);
+            Assert.AreEqual("teststring", data.Value);
+
+        }
+
+        [Test]
+        public void BasicAuth()
+        {
+            string authdata = "Basic " + Base64.Default.Encode("user:password");
+            HttpWebRequest request = System.Net.WebRequest.CreateHttp("http://localhost:8080/basicauth");
+            request.Headers.Add("Authorization", authdata);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
     }
 }
